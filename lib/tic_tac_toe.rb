d78b6b55ef316ab45c38127a36ec96f900391294
Ishani <ishaniamin@gmail.com>
2018-05-31 19:20:01 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,9 +32,6 @@ def current_player(board)
 end
 
 
-# def move(board, index, current_player = "X")
-#   board[index] = current_player
-# end
 def move(board, index, player)
   board[index] = player
 end
